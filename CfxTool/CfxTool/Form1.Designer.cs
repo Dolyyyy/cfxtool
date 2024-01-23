6e66a6f69e1c86aab4e40dc1e018a7b97e7b5bda
Dolyyyy <66006040+Dolyyyy@users.noreply.github.com>
2024-01-23 22:41:25 +0100

    feat: first tools

---
+++ CfxTool/CfxTool/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CfxTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RedmToAzerty = new System.Windows.Forms.Button();
+            this.ClearCacheRedm = new System.Windows.Forms.Button();
+            this.ClearCacheFivem = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.LaunchFivem = new System.Windows.Forms.Button();
+            this.LaunchRedm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RedmToAzerty
+            // 
+            resources.ApplyResources(this.RedmToAzerty, "RedmToAzerty");
+            this.RedmToAzerty.Name = "RedmToAzerty";
+            this.RedmToAzerty.UseVisualStyleBackColor = true;
+            this.RedmToAzerty.Click += new System.EventHandler(this.RedmToAzerty_Click);
+            // 
+            // ClearCacheRedm
+            // 
+            resources.ApplyResources(this.ClearCacheRedm, "ClearCacheRedm");
+            this.ClearCacheRedm.Name = "ClearCacheRedm";
+            this.ClearCacheRedm.UseVisualStyleBackColor = true;
+            this.ClearCacheRedm.Click += new System.EventHandler(this.ClearCacheRedm_Click);
+            // 
+            // ClearCacheFivem
+            // 
+            resources.ApplyResources(this.ClearCacheFivem, "ClearCacheFivem");
+            this.ClearCacheFivem.Name = "ClearCacheFivem";
+            this.ClearCacheFivem.UseVisualStyleBackColor = true;
+            this.ClearCacheFivem.Click += new System.EventHandler(this.ClearCacheFivem_Click);
+            // 
+            // maskedTextBox1
+            // 
+            resources.ApplyResources(this.maskedTextBox1, "maskedTextBox1");
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.ReadOnly = true;
+            // 
+            // maskedTextBox2
+            // 
+            resources.ApplyResources(this.maskedTextBox2, "maskedTextBox2");
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.ReadOnly = true;
+            // 
+            // LaunchFivem
+            // 
+            resources.ApplyResources(this.LaunchFivem, "LaunchFivem");
+            this.LaunchFivem.Name = "LaunchFivem";
+            this.LaunchFivem.UseVisualStyleBackColor = true;
+            this.LaunchFivem.Click += new System.EventHandler(this.LaunchFivem_Click);
+            // 
+            // LaunchRedm
+            // 
+            resources.ApplyResources(this.LaunchRedm, "LaunchRedm");
+            this.LaunchRedm.Name = "LaunchRedm";
+            this.LaunchRedm.UseVisualStyleBackColor = true;
+            this.LaunchRedm.Click += new System.EventHandler(this.LaunchRedm_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LaunchRedm);
+            this.Controls.Add(this.LaunchFivem);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.ClearCacheFivem);
+            this.Controls.Add(this.ClearCacheRedm);
+            this.Controls.Add(this.RedmToAzerty);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RedmToAzerty;
+        private System.Windows.Forms.Button ClearCacheRedm;
+        private System.Windows.Forms.Button ClearCacheFivem;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button LaunchFivem;
+        private System.Windows.Forms.Button LaunchRedm;
+    }
+}
+
---
+++ CfxTool/CfxTool/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace CfxTool
+{
+    public partial class Form1 : Form
+    {
+        private string ComputerUsername = null;
+        private string DefaultRedmPath = null;
+        private string DefaultFivemPath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComputerUsername = Environment.UserName;
+            DefaultRedmPath = Path.Combine("C:\\Users", ComputerUsername, "AppData", "Local", "RedM");
+            DefaultFivemPath = Path.Combine("C:\\Users", ComputerUsername, "AppData", "Local", "FiveM");
+        }
+
+        private void RedmToAzerty_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string DefaultMetaPath = Path.Combine(DefaultRedmPath, "RedM.app", "citizen", "common", "data", "control", "default.meta");
+
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://cdn.codoly.fr/p/default.meta", DefaultMetaPath);
+                }
+
+                MessageBox.Show("Le fichier default.meta a été remplacé avec succès. Votre jeu sera désormais en AZERTY.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du téléchargement du fichier : {ex.Message}.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearCacheRedm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string DefaultRedmCachePath = Path.Combine(DefaultRedmPath, "RedM.app", "data");
+                foreach (var folder in Directory.GetDirectories(DefaultRedmCachePath))
+                {
+                    if (Path.GetFileName(folder) != "game-storage")
+                    {
+                        Directory.Delete(folder, true);
+                    }
+                }
+
+                MessageBox.Show("Cache RedM nettoyé avec succès.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du vidage du cache RedM : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void ClearCacheFivem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string DefaultFivemCachePath = Path.Combine(DefaultFivemPath, "RedM.app", "data");
+
+                foreach (var folder in Directory.GetDirectories(DefaultFivemCachePath))
+                {
+                    if (Path.GetFileName(folder) != "game-storage")
+                    {
+                        Directory.Delete(folder, true);
+                    }
+                }
+
+                MessageBox.Show("Cache FiveM nettoyé avec succès.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du vidage du cache FiveM : {ex.Message}.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LaunchFivem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string FiveMExecutablePath = Path.Combine(DefaultFivemPath, "FiveM.exe");
+                if (File.Exists(FiveMExecutablePath))
+                {
+                    Process.Start(FiveMExecutablePath);
+                }
+                else
+                {
+                    MessageBox.Show("Le fichier exécutable FiveM n'a pas été trouvé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du démarrage de l'application FiveM : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LaunchRedm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string RedMExecutablePath = Path.Combine(DefaultRedmPath, "RedM.exe");
+                if (File.Exists(RedMExecutablePath))
+                {
+                    Process.Start(RedMExecutablePath);
+                }
+                else
+                {
+                    MessageBox.Show("Le fichier exécutable RedM n'a pas été trouvé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du démarrage de l'application RedM : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
